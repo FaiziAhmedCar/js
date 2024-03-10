6ad1b0f9351d6aa6a8ee59bfa3265762635bf08e
Faizi_Ahmed <125595034+FaiziAhmedCar@users.noreply.github.com>
2024-03-10 21:35:50 +0530

    Add files via upload

---
+++ Java Script/button.js
@@ -0,0 +1,8 @@
+// let  myName = document.getElementById('#name')
+let subButton = document.querySelector('#myButton');
+subButton.addEventListener('click',showMsg);
+
+function showMsg(){
+    // alert(myName)
+alert("lol it works");
+}
---
+++ Java Script/object.js
@@ -0,0 +1,16 @@
+let course={
+    lecture:10,
+    section:3,
+    title:`javeScript`,
+
+    note:{
+        introduction:`welcome to js class`
+    },
+
+    enrole() {
+
+        console.log(`you are sucessfully enrolled`);
+    }
+}
+// course.enrole()
+console.log(course)
